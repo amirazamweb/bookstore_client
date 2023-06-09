531dd1b2bef3ebcdddb216ffcaef09c1257da1f1
amir azam <amirazam281@gmail.com>
2023-06-09 16:48:25 +0530

    changed

--- src/pages/CategoryProducts.jsx
+++ src/pages/CategoryProducts.jsx
@@ -4,18 +4,23 @@ import { useParams } from 'react-router-dom';
 import Card from '../components/Card';
 import axios from 'axios';
 import notFoundAnimation from '../images/not-found-animation.gif';
+import loadingImg from '../images/loading.gif'
 
 const CategoryProducts = () => {
     const { slug } = useParams();
     const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     // get category product
     const getCategoryProduct = async () => {
+        setLoading(true);
         try {
             const { data } = await axios.get(`${process.env.REACT_APP_SERVER_DOMAIN}/api/v1/product/category/${slug}`);
             setProducts(data?.products);
+            setLoading(false);
         } catch (error) {
             console.log(error);
+            setLoading(false);
         }
     }
 
@@ -27,35 +32,43 @@ const CategoryProducts = () => {
     return (
         <Layout>
             {
-                products.length > 0 ?
-                    (<div>
-                        <h2
-                            className='text-center text-3xl font-semibold text-slate-500 my-8'
-                        >{products[0]?.category}</h2>
-                        <div className='grid grid-cols-5 gap-12 px-24'>
-                            {
-                                products.map((el) => (
-                                    <Card
-                                        key={el._id}
-                                        name={el.name}
-                                        author={el.author}
-                                        price={el.price}
-                                        id={el._id}
-                                        slug={el.productSlug}
-                                        cardInfo={el}
-                                    />
-                                ))
-                            }
-                        </div>
-                    </div>) :
-
+                loading ?
                     (
-                        <div className='text-center my-8'>
-                            <h2 className='text-xl font-semibold text-slate-500'>No product found</h2>
-                            <div className='w-1/3 m-auto my-4'>
-                                <img src={notFoundAnimation} alt="not product found" className='w-full' />
-                            </div>
+                        <div className='w-1/5 m-auto py-32'>
+                            <img src={loadingImg} alt="loading image" className='w-full' />
                         </div>
+                    ) :
+                    (
+                        products.length > 0 ?
+                            (<div>
+                                <h2
+                                    className='text-center text-3xl font-semibold text-slate-500 my-8'
+                                >{products[0]?.category}</h2>
+                                <div className='grid grid-cols-5 gap-12 px-24'>
+                                    {
+                                        products.map((el) => (
+                                            <Card
+                                                key={el._id}
+                                                name={el.name}
+                                                author={el.author}
+                                                price={el.price}
+                                                id={el._id}
+                                                slug={el.productSlug}
+                                                cardInfo={el}
+                                            />
+                                        ))
+                                    }
+                                </div>
+                            </div>) :
+
+                            (
+                                <div className='text-center my-8'>
+                                    <h2 className='text-xl font-semibold text-slate-500'>No product found</h2>
+                                    <div className='w-1/3 m-auto my-4'>
+                                        <img src={notFoundAnimation} alt="not product found" className='w-full' />
+                                    </div>
+                                </div>
+                            )
                     )
             }
         </Layout>
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -7,6 +7,7 @@ import axios from 'axios';
 import notFoundAnimation from '../images/not-found-animation.gif';
 import { useCart } from '../context/cart';
 import toast from 'react-hot-toast';
+import loadingImg from '../images/loading.gif'
 
 const ProductDetail = () => {
     const { slug } = useParams();
@@ -14,6 +15,7 @@ const ProductDetail = () => {
     const [similarProducts, setSimilarProducts] = useState([]);
     const [count, setCount] = useState(2);
     const [cart, setCart] = useCart();
+    const [loading, setLoading] = useState(true);
 
     // decreament Handler
     const decreamentHandler = () => {
@@ -53,11 +55,14 @@ const ProductDetail = () => {
 
     // get similar products
     const getSimilarProducts = async () => {
+        setLoading(true);
         try {
             const { data } = await axios.get(`${process.env.REACT_APP_SERVER_DOMAIN}/api/v1/product/similar-products/${slug}`);
             setSimilarProducts(data?.products);
+            setLoading(false);
         } catch (error) {
             console.log(error);
+            setLoading(false);
         }
     }
 
@@ -69,75 +74,86 @@ const ProductDetail = () => {
 
     return (
         <Layout title={slug}>
-            <div className='flex gap-20 w-3/4 m-auto mt-12'>
-                <div className='w-1/3'>
-                    <img src={`${process.env.REACT_APP_SERVER_DOMAIN}/api/v1/product/photo/${productInfo?._id}`}
-                        alt="product-details" className='w-full' />
-                </div>
-                <div className='w-2/3'>
-                    <h1 className='text-4xl font-semi-bold text-slate-500'>{productInfo?.name}</h1>
-                    <p
-                        className='border-solid border-2 border-slate-300 w-1/3 rounded px-2 py-1 my-4 text-slate-500'>English</p>
-                    <div className='flex items-center my-5'>
-                        <p className='text-slate-500 font-bold text-xl me-8'>Quantity: </p>
-                        <div className='flex items-center w-1/5 justify-between text-2xl font-bold'>
-                            <GrFormSubtract
-                                className='outline outline-2 outline-slate-300 rounded hover:bg-red-500 w-8 h-8 cursor-pointer'
-                                onClick={decreamentHandler} />
-                            <span>{count}</span>
-                            <GrFormAdd
-                                className='outline outline-2 outline-slate-300 rounded hover:bg-green-500 w-8 h-8 cursor-pointer'
-                                onClick={increamentHandler} />
-                        </div>
+            {
+                loading ? (
+                    <div className='w-1/5 m-auto py-32'>
+                        <img src={loadingImg} alt="loading image" className='w-full' />
                     </div>
-                    <div className='border-solid border-2 border-slate-300 w-1/3 rounded px-3 py-1 my-4'>
-                        <h4 className='text-slate-500'>Paperback</h4>
-                        <p>
-                            <span className='text-red-500 font-bold text-xl'>&#8377;{productInfo?.price}</span>
-                            <del className='text-slate-400 mx-1'>&#8377;123</del>
-                            <span className='text-green-500'>10% OFF</span>
-                        </p>
-                    </div>
-                    <div className='my-2'>
-                        <button
-                            className='bg-red-500 text-white rounded px-3 py-2 font-semibold'
-                            onClick={addToCartHandler}>GO TO CART</button>
-                    </div>
-                    <div>
-                        <h3 className='text-xl font-semibold text-salte-600 my-2'>Description</h3>
-                        <p className='text-sm text-slate-500'>{productInfo?.description}</p>
-                    </div>
-                </div>
-            </div>
-
-            {/* similar products */}
-            <hr className='my-3' />
-            <div>
-                <h2 className='text-center text-3xl font-semibold text-slate-500 my-4'>Similar Products</h2>
-                <div className='grid grid-cols-5 gap-12 px-24'>
-                    {
-                        similarProducts.length > 0 ?
-                            (
-                                similarProducts.map((elm) => (
-                                    <Card
-                                        key={elm._d}
-                                        name={elm.name}
-                                        author={elm.author}
-                                        price={elm.price}
-                                        id={elm._id}
-                                        slug={elm.productSlug}
-                                        cardInfo={elm}
-                                    />
-                                ))
-                            ) :
-                            (
-                                <div className='w-1/3 m-auto my-12'>
-                                    <img src={notFoundAnimation} alt="not found" />
+                ) :
+                    (
+                        <>
+                            <div className='flex gap-20 w-3/4 m-auto mt-12'>
+                                <div className='w-1/3'>
+                                    <img src={`${process.env.REACT_APP_SERVER_DOMAIN}/api/v1/product/photo/${productInfo?._id}`}
+                                        alt="product-details" className='w-full' />
                                 </div>
-                            )
-                    }
-                </div>
-            </div>
+                                <div className='w-2/3'>
+                                    <h1 className='text-4xl font-semi-bold text-slate-500'>{productInfo?.name}</h1>
+                                    <p
+                                        className='border-solid border-2 border-slate-300 w-1/3 rounded px-2 py-1 my-4 text-slate-500'>English</p>
+                                    <div className='flex items-center my-5'>
+                                        <p className='text-slate-500 font-bold text-xl me-8'>Quantity: </p>
+                                        <div className='flex items-center w-1/5 justify-between text-2xl font-bold'>
+                                            <GrFormSubtract
+                                                className='outline outline-2 outline-slate-300 rounded hover:bg-red-500 w-8 h-8 cursor-pointer'
+                                                onClick={decreamentHandler} />
+                                            <span>{count}</span>
+                                            <GrFormAdd
+                                                className='outline outline-2 outline-slate-300 rounded hover:bg-green-500 w-8 h-8 cursor-pointer'
+                                                onClick={increamentHandler} />
+                                        </div>
+                                    </div>
+                                    <div className='border-solid border-2 border-slate-300 w-1/3 rounded px-3 py-1 my-4'>
+                                        <h4 className='text-slate-500'>Paperback</h4>
+                                        <p>
+                                            <span className='text-red-500 font-bold text-xl'>&#8377;{productInfo?.price}</span>
+                                            <del className='text-slate-400 mx-1'>&#8377;123</del>
+                                            <span className='text-green-500'>10% OFF</span>
+                                        </p>
+                                    </div>
+                                    <div className='my-2'>
+                                        <button
+                                            className='bg-red-500 text-white rounded px-3 py-2 font-semibold'
+                                            onClick={addToCartHandler}>GO TO CART</button>
+                                    </div>
+                                    <div>
+                                        <h3 className='text-xl font-semibold text-salte-600 my-2'>Description</h3>
+                                        <p className='text-sm text-slate-500'>{productInfo?.description}</p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            {/* similar products */}
+                            <hr className='my-3' />
+                            <div>
+                                <h2 className='text-center text-3xl font-semibold text-slate-500 my-4'>Similar Products</h2>
+                                <div className='grid grid-cols-5 gap-12 px-24'>
+                                    {
+                                        similarProducts.length > 0 ?
+                                            (
+                                                similarProducts.map((elm) => (
+                                                    <Card
+                                                        key={elm._d}
+                                                        name={elm.name}
+                                                        author={elm.author}
+                                                        price={elm.price}
+                                                        id={elm._id}
+                                                        slug={elm.productSlug}
+                                                        cardInfo={elm}
+                                                    />
+                                                ))
+                                            ) :
+                                            (
+                                                <div className='w-1/3 m-auto my-12'>
+                                                    <img src={notFoundAnimation} alt="not found" />
+                                                </div>
+                                            )
+                                    }
+                                </div>
+                            </div>
+                        </>
+                    )
+            }
         </Layout>
     )
 }
