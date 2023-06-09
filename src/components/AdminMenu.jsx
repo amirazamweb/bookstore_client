dc57ef3dea129e3fddd7321df958c672aa8930a4
amir azam <amirazam281@gmail.com>
2023-06-09 15:07:29 +0530

    changed

--- src/components/AdminMenu.jsx
+++ src/components/AdminMenu.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 const AdminMenu = () => {
     return (
         <div className='fixed top-20 left-20'>
-            <Link to='/dashboard/admin/profile' ><div className='mb-4 bg-slate-500 text-white'>Profile</div></Link>
-            <Link to='/dashboard/admin/create-product'><div className='mb-4 bg-slate-500 text-white'>Create Product</div></Link>
+            <Link to='/dashboard/admin/profile' ><div className='mb-4 bg-slate-500 text-white px-2 py-1'>Profile</div></Link>
+            <Link to='/dashboard/admin/create-product'><div className='mb-4 bg-slate-500 text-white px-2 py-1'>Create Product</div></Link>
         </div>
     )
 }
